4fdc31034df8cb0d72398d734d4a54a805259912
bydoodle <82917218+bydoodle@users.noreply.github.com>
2026-08-01 16:50:27 +0200

    link to reddit changed to a newer post

--- website/src/App.jsx
+++ website/src/App.jsx
@@ -199,7 +199,7 @@ function App() {
           <a href="https://steamcommunity.com/id/doodlehateu/" target='_blank' rel="noopener noreferrer">
             <img src={steam} className='size-10 xl:size-12 rounded-full' alt="Steam"/>
           </a>
-          <a href="https://www.reddit.com/r/DotA2/comments/1vbt4z3/fantasy_calculator_2026/" target='_blank' rel="noopener noreferrer">
+          <a href="https://www.reddit.com/r/DotA2/comments/1vcpcvr/update_fantasy_league_2026_calculator/" target='_blank' rel="noopener noreferrer">
             <img src={reddit} className='size-10 xl:size-12 rounded-full' alt="Reddit"/>
           </a>
           <a href="https://github.com/bydoodle/dota2fantasy" target="_blank" rel="noopener noreferrer">
